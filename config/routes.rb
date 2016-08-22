c84bfaec2872d7f3fb90fb1d9bbf7771254dbe8d
ilya <dubovtsev98@gmail.com>
2016-08-22 22:52:06 +0400

    +pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :projects
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 
   root to: "projects#index"
   # The priority is based upon order of creation: first created -> highest priority.
