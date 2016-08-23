3c0f0455d986c900d4d633f6c0c362a2adb3f328
ilya <dubovtsev98@gmail.com>
2016-08-23 14:58:03 +0400

    +about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users
   resources :projects
   get 'about' => 'static_pages#about'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822195138) do
+ActiveRecord::Schema.define(version: 20160823104439) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "text"
+    t.string   "url"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.string   "logo_file_name"
+    t.string   "logo_content_type"
+    t.integer  "logo_file_size"
+    t.datetime "logo_updated_at"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.datetime "created_at",           null: false
